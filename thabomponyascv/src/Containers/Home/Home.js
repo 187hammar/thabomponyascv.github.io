68a06de6844217fa488bc631ccabd3636a10d4ca
Thabo <tmponya187@hotmail.com>
2020-10-02 19:19:28 +0200

    Last background update

--- thabomponyascv/src/Containers/Home/Home.js
+++ thabomponyascv/src/Containers/Home/Home.js
@@ -5,19 +5,25 @@ import HomeBackground from './HomeBackground/HomeBackground';
 
 const Home = () => {
     return (
-        <div className="Home">
-            <HomeBackground className="HomeBackground"/>
+        <div>
+            <div className="Home">
+                <HomeBackground className="HomeBackground"/>
+                <div className={"Home bg1"}></div>
+                <div className={"Home bg2"}></div>
             <div className="d5">
                 <h1>Hello,</h1>
                 <p>I am Thabo Mponya, software developer from Potchefstroom, South Africa. 
                     I am skilled in mobile app development, software development and currently learning web development. 
                     I have a variety of programming languages and frameworks that I am skilled in.</p>
-                <p className={"p2"}>From the beginning of the South African national lockdown, I earned 2 certificates (Flutter & Dart - The complete guide [2020 edition] and Ethical Hacking from Scratch),
+                <p>From the beginning of the South African national lockdown, I earned 2 certificates (Flutter & Dart - The complete guide [2020 edition] and Ethical Hacking from Scratch),
                      and currently working on finishing the remaining 2 (React - The Complete Guide (incl Hooks, React Router, Redux) and React & Django Full Stack: web app, backend API, mobile apps). 
                     I did all this before I got called back to the university.
                     I am a very quick learner and am highly interested in all types software Development, incluiding mobile app and website development.
                 </p>
             </div>
+            </div>
+
+            
         </div>
     )
 }
