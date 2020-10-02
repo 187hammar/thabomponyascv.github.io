7ee22f99f7cc365dc15825ca6ad59e26a57a123b
Thabo <tmponya187@hotmail.com>
2020-10-02 16:39:45 +0200

    16:39 update

--- thabomponyascv/src/Components/Navigation/SideBar/SideBar.js
+++ thabomponyascv/src/Components/Navigation/SideBar/SideBar.js
@@ -4,9 +4,8 @@ import facebook from '../../../Assets/FB.png';
 import twitter from '../../../Assets/T.png';
 import instagram from '../../../Assets/I.png';
 import linkedIn from '../../../Assets/IN.png';
-import NavigationItem from '../NavigationItem';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideBar.css';
+import './SideBar.css';
 import Profile from '../../Profile/Profile';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../../BackDrop/Backdrop';
@@ -28,17 +27,17 @@ const sideDrawer = (props) => {
                             <NavigationItems/>
                         </div>
                     <div className={"socialMediaIcons"}>
-                        <a href="">
+                        <a href="https://www.facebook.com/XTeddyBear187Mp">
                             <img className={"Icon"} src={facebook} alt="facebook icon" />
                         </a>
-                        <a href="">
+                        <a href="https://www.instagram.com/materialteddybear/">
                             <img className={"Icon"} src={instagram} alt="instagram icon" />
                         </a>
-                        <a href="">
+                        <a href="https://twitter.com/ARtisTXda187">
                             <img className={"Icon"} src={twitter} alt="twitter icon" />
                         </a>
-                        <a href="">
-                            <img className={"Icon"} src={linkedIn} alt="twitter icon" />
+                        <a href="https://linkedin.com/in/thabo-mponya-6538a219b">
+                            <img className={"Icon"} src={linkedIn} alt="linkedIn icon" />
                         </a>
                     </div>
                 </div>
