55d7c7f87068dcdf28c4bba662622d6d5ba55b6b
Thabo <tmponya187@hotmail.com>
2020-10-02 10:21:12 +0200

    online backgrounds

--- thabomponyascv/src/Containers/Home/HomeBackground/HomeBackground.js
+++ thabomponyascv/src/Containers/Home/HomeBackground/HomeBackground.js
@@ -12,22 +12,22 @@ const homeBackground = () => {
     return (
         <div className="HomeBackground">
             <div className="d1">
-                <img className="img1" src={ntate} alt="ntate"></img>
+                <img className="img1" src={"https://firebasestorage.googleapis.com/v0/b/memorypics-cd4a8.appspot.com/o/ThaboMponya%2FNtate_Mponya.jpeg?alt=media&token=f4ff91ca-250f-400a-9023-1cf0f4b265fc"} alt="ntate"></img>
             </div>
             <div className="d2">
-                <img className="img2" src={Thabo3} alt="Thabo3"></img>
+                <img className="img2" src={"https://firebasestorage.googleapis.com/v0/b/memorypics-cd4a8.appspot.com/o/ThaboMponya%2FThabo3x.jpg?alt=media&token=05cf3bbb-6698-467b-a105-00447479cb2d"} alt="Thabo3"></img>
             </div>
             <div className="d21">
-                <img className="img21" src={tnK} alt="Thabo5"></img>
+                <img className="img21" src={"https://firebasestorage.googleapis.com/v0/b/memorypics-cd4a8.appspot.com/o/ThaboMponya%2FThabo5x.jpg?alt=media&token=3d38749f-34e5-42a4-ba9d-5b2b83436345"} alt="Thabo5"></img>
             </div>
             <div className="d3">
-                <img src={Thabo} alt="Thabo"></img>
+                <img src={"https://firebasestorage.googleapis.com/v0/b/memorypics-cd4a8.appspot.com/o/ThaboMponya%2FThabo.jpg?alt=media&token=6d8f26d8-b255-455c-bc04-204c4ae96241"} alt="Thabo"></img>
             </div>
             <div className="d31">
-                <img src={Thabo4} className="img31" alt="Thabo4"></img>
+                <img src={"https://firebasestorage.googleapis.com/v0/b/memorypics-cd4a8.appspot.com/o/ThaboMponya%2FThabo4x.jpg?alt=media&token=3ead50c9-3773-49a6-83d7-b5953164e1ce"} className="img31" alt="Thabo4"></img>
             </div>
             <div className="d4">
-                <img className="img4" src={Thabo2} alt="Thabo2"></img>
+                <img className="img4" src={"https://firebasestorage.googleapis.com/v0/b/memorypics-cd4a8.appspot.com/o/ThaboMponya%2FThabo2.jpg?alt=media&token=1cb1b82d-1265-4c3e-a3ff-cc2821e1e2db"} alt="Thabo2"></img>
             </div>
         </div>
     )
